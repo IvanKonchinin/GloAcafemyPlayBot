2a43fd2cdb2a9bbb56707cb574aedadaf42d7343
Ivan Konchinin <vanish7@mail.ru>
2020-08-13 21:21:12 +0300

    fix: change functional in js/script.js

--- js/script.js
+++ js/script.js
@@ -3,51 +3,51 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-function getRandomInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+function quessNumber() {
 
+  let randomNumber = Math.floor(Math.random() * 100) + 1;
+  let count = 10;
 
-function quessNumber(randomNumber, count) {
-  
-  let userNumber = prompt('Угадай число от 1 до 100');
-   if (count <= 0) {
-    alert('Попытки закончились!');
-    if (confirm('Хотите сыграть еще!')) {
-      quessNumber(getRandomInRange(1, 100), 10);
-    } else {
+  function bot(randomNumber, count) {
+    let userNumber = prompt('Угадай число от 1 до 100');
+
+    if (count <= 0) {
+      alert('Попытки закончились!');
+      if (confirm('Хотите сыграть еще!')) {
+        quessNumber();
+      } else {
+        alert('До свидания');
+        return;
+      }
+    }
+    else if (userNumber === null) {
       alert('До свидания');
       return;
-    }
-  }
-  else if(userNumber === null){
-     alert('До свидания');
-    return;
-  } 
-  else if(!isNumber(userNumber)){
+    } 
+    else if(!isNumber(userNumber)){
     alert('Введите число!');
-    quessNumber(randomNumber, count);
-  } 
- 
-  else if(randomNumber < userNumber){
+    bot(randomNumber, count);
+    } 
+    else if(randomNumber < userNumber){
     count--;
     alert('Загаданное число меньше, осталось: ' + count + ' попыток');
-    quessNumber(randomNumber, count);
-  } 
-  else if(randomNumber > userNumber){
+    bot(randomNumber, count);
+    }
+    else if(randomNumber > userNumber){
     count--;
     alert('Загаданное число больше, осталось: ' + count + ' попыток');
-    quessNumber(randomNumber, count);
-  }
-  else {
-    alert('Поздравляю, вы угадали!!!');
-    if(confirm('Хотите сыграть еще?')){
-      quessNumber(getRandomInRange(1, 100), 10);
-    }else{
-      alert('До свидания');
-      return;
+    bot(randomNumber, count);
+    } else {
+      alert('Поздравляю, вы угадали!!!');
+      if (confirm('Хотите сыграть еще?')) {
+        quessNumber();
+      } else {
+        alert('До свидания');
+        return;
+      }
     }
   }
+  return bot(randomNumber, count);
 }
 
-quessNumber(getRandomInRange(1, 100) , 10);
+quessNumber();
